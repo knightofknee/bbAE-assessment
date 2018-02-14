81ed13fc484e524c919444005d2e829e08151d4e
knightofknee <bricarlis@gmail.com>
2018-02-14 16:50:21 -0600

    done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 function navBarClick() {
-  console.log('hello!!!')
+
   let navElement = document.getElementById('header-bot');
   if (navElement.className === 'header-bottom') {
       navElement.className += ' activated';
@@ -8,4 +8,34 @@ function navBarClick() {
       navElement.className = 'header-bottom';
   }
   return false
+}
+
+function accordionAbout(){
+  let subsection = document.getElementById('about-us')
+  if (subsection.className === 'expander__panel') {
+    subsection.className += ' clicked';
+} else {
+    subsection.className = 'expander__panel';
+}
+return false
+}
+
+function accordionContact(){
+  let subsection = document.getElementById('contact')
+  if (subsection.className === 'expander__panel') {
+    subsection.className += ' clicked';
+} else {
+    subsection.className = 'expander__panel';
+}
+return false
+}
+
+function accordionNav(){
+  let subsection = document.getElementById('acc-nav')
+  if (subsection.className === 'expander__panel') {
+    subsection.className += ' clicked';
+} else {
+    subsection.className = 'expander__panel';
+}
+return false
 }
